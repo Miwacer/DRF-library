b03be02d44d49066dd9f5bcf0d51b0c0bfc17e92
Miwacer <mixanavvy@gmail.com>
2025-07-18 00:22:11 +0300

    feat(Book): Add model

--- library/models.py
+++ library/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class CoverType(models.TextChoices):
+    HARD = "HD", "Hard"
+    SOFT = "ST", "Soft"
+
+
+class Book(models.Model):
+    title = models.CharField(unique=True, null=False, max_length=255)
+    # author = models.ManyToManyField("Author", related_name="books") to do
+    cover = models.CharField(
+        max_length=2,
+        choices=CoverType.choices,
+    )
+    inventory = models.IntegerField()
+    daily_fee = models.DecimalField(max_digits=10, decimal_places=2)
