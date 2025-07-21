411d0847ad18c6b973cca2ed5681146ef6436a7a
Miwacer <mixanavvy@gmail.com>
2025-07-21 23:46:09 +0300

    feat(Borrowing): Create model

--- library/models.py
+++ library/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
 
@@ -15,3 +16,16 @@ class Book(models.Model):
     )
     inventory = models.IntegerField()
     daily_fee = models.DecimalField(max_digits=10, decimal_places=2)
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateTimeField(null=False)
+    expected_return_date = models.DateTimeField(null=False)
+    actual_return_date = models.DateTimeField(null=True, blank=True)
+
+    book = models.ForeignKey(
+        "Book", on_delete=models.CASCADE, related_name="borrowings"
+    )
+    user = models.ForeignKey(
+        get_user_model(), on_delete=models.CASCADE, related_name="borrowings"
+    )
