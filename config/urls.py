9feb1834431bd9395323d65a80adf01c92d3bb3c
Miwacer <mixanavvy@gmail.com>
2025-07-26 10:37:48 +0300

    feat: add global paginations

--- config/urls.py
+++ config/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/v1/book/", include("book.urls", namespace="book")),
-    path("api/v1/borrowing/", include("borrowing.urls", namespace="borrowing")),
+    path("api/v1/books/", include("book.urls", namespace="book")),
+    path("api/v1/borrowings/", include("borrowing.urls", namespace="borrowing")),
     path("api/v1/user/", include("user.urls", namespace="user")),
 ]
